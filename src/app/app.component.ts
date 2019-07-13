6594d9a5ff72f2c0ec5ef5e0fdd4bc824605c19b
Leung Wong <wguoliang1113@gmail.com>
2019-07-14 00:22:00 +0200

    Adds line chart with fake data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ids-graph';
-  
+
+  lineData = [{
+    data: [
+      {
+        "id": 0,
+        "name": "2014-03-23",
+        "value": 221
+      },
+      {
+        "id": 1,
+        "name": "2017-10-24",
+        "value": 171
+      },
+      {
+        "id": 2,
+        "name": "2015-02-16",
+        "value": 109
+      },
+      {
+        "id": 3,
+        "name": "2018-08-28",
+        "value": 100
+      },
+      {
+        "id": 4,
+        "name": "2014-03-31",
+        "value": 357
+      },
+      {
+        "id": 5,
+        "name": "2017-12-26",
+        "value": 377
+      },
+      {
+        "id": 6,
+        "name": "2015-01-18",
+        "value": 400
+      },
+      {
+        "id": 7,
+        "name": "2017-04-16",
+        "value": 312
+      },
+      {
+        "id": 8,
+        "name": "2017-12-03",
+        "value": 307
+      },
+      {
+        "id": 9,
+        "name": "2016-06-24",
+        "value": 260
+      }
+    ],
+    name: "Fake data"
+  }];
+
   constructor() {
     Soho.Locale.culturesPath = '/assets/ids-enterprise/js/cultures/';
     Soho.Locale.set('en-US');
