75d68edd693bfd34943da2627529d0f1cb4a7396
Leung Wong <leung.wong@infor.com>
2019-07-14 23:35:20 +0200

    Use http to fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, ViewChild, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+
+import { SohoLineComponent, SohoChartComponent } from 'ids-enterprise-ng';
+
+export interface IChartData {
+  id: number;
+  name: string;
+  value: number;
+}
+export interface IChartDataSet {
+  data: IChartData[];
+  name: string;
+}
+
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild(SohoLineComponent, { static: false })
+  sohoLineComponent: SohoLineComponent;
+
   title = 'ids-graph';
 
-  lineData = [{
-    data: [
-      {
-        "id": 0,
-        "name": "2014-03-23",
-        "value": 221
-      },
-      {
-        "id": 1,
-        "name": "2017-10-24",
-        "value": 171
-      },
-      {
-        "id": 2,
-        "name": "2015-02-16",
-        "value": 109
-      },
-      {
-        "id": 3,
-        "name": "2018-08-28",
-        "value": 100
-      },
-      {
-        "id": 4,
-        "name": "2014-03-31",
-        "value": 357
-      },
-      {
-        "id": 5,
-        "name": "2017-12-26",
-        "value": 377
-      },
-      {
-        "id": 6,
-        "name": "2015-01-18",
-        "value": 400
-      },
-      {
-        "id": 7,
-        "name": "2017-04-16",
-        "value": 312
-      },
-      {
-        "id": 8,
-        "name": "2017-12-03",
-        "value": 307
-      },
-      {
-        "id": 9,
-        "name": "2016-06-24",
-        "value": 260
-      }
-    ],
-    name: "Fake data"
-  }];
+  lineData: IChartDataSet[];
 
-  constructor() {
+  constructor(
+    private http: HttpClient
+  ) {
     Soho.Locale.culturesPath = '/assets/ids-enterprise/js/cultures/';
     Soho.Locale.set('en-US');
   }
+
   ngOnInit(): void {
-    console.log("Hello World");
+    if (!this.lineData) {
+      this.lineData = [];
+    }
+
+    this.getFakeData().subscribe(data => {
+      this.lineData.push(this.parseData(data, 'Fake Data 2'));
+    });
+  }
+  parseData(
+    value: { id: number; date: string; production: number }[],
+    name: string
+  ): IChartDataSet {
+
+    const data: IChartData[] = value.map(item => {
+      return { id: item.id, name: item.date, value: item.production };
+    });
+
+    return {
+      data,
+      name
+    };
+  }
+
+  getFakeData(): Observable<[]> {
+    const url = 'fake-data/generated.json';
+
+    return this.http.get<[]>(url);
   }
 }
